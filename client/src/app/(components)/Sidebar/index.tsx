87d2e65e8a88e3b6145dedcd8dfbe52c2589e5bc
Caio <caiomarianni@gmail.com>
2024-10-22 14:49:48 -0300

    Creation of page links and changes depending on the page the user is on

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,8 +1,59 @@
 "use client";
+import Dashboard from "@/app/dashboard/page";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/state";
-import { Menu } from "lucide-react";
+import {
+  Archive,
+  CircleDollarSign,
+  Clipboard,
+  Layout,
+  LucideIcon,
+  Menu,
+  SlidersHorizontal,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollpased: boolean;
+}
+// To highlight the page the user is
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollpased,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center ${
+          isCollpased ? "justify-center py-4" : "justify-start px-8 py-4"
+        } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          isActive ? "bg-blue-200 text-white" : ""
+        }`}
+      >
+        <Icon className="w-6 h-6 !text-gray-700" />
+        <span
+          className={`${
+            isCollpased ? "hidden" : "block"
+          } font-medium text-gray-700`}
+        >
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 const Sidebar = () => {
   const dispatch = useAppDispatch();
@@ -19,9 +70,19 @@ const Sidebar = () => {
   return (
     <div className={sidebarClassNames}>
       {/* Top logo */}
-      <div className={`flex justify-between md:justify-normal items-center gap-3 pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
+      <div
+        className={`flex justify-between md:justify-normal items-center gap-3 pt-8 ${
+          isSidebarCollapsed ? "px-5" : "px-8"
+        }`}
+      >
         <div>Logo</div>
-        <h1 className={`${isSidebarCollapsed ? "hidden" : "block" } font-extrabold text-2xl`}>EDSTOCK</h1>
+        <h1
+          className={`${
+            isSidebarCollapsed ? "hidden" : "block"
+          } font-extrabold text-2xl`}
+        >
+          EDSTOCK
+        </h1>
         <button
           onClick={toggleSidebar}
           className="md:hidden p-3 bg-gray-100 rounded-full hover:bg-blue-100"
@@ -30,10 +91,53 @@ const Sidebar = () => {
         </button>
       </div>
       {/* Links */}
-      <div className="flex-grow mt-8">{/* LINKS HERE */}</div>
+      <div className="flex-grow mt-8">
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="Dashboard"
+          isCollpased={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/inventory"
+          icon={Archive}
+          label="Inventory"
+          isCollpased={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/products"
+          icon={Clipboard}
+          label="Products"
+          isCollpased={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/users"
+          icon={User}
+          label="Users"
+          isCollpased={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/settings"
+          icon={SlidersHorizontal}
+          label="Settings"
+          isCollpased={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/expenses"
+          icon={CircleDollarSign}
+          label="Expenses"
+          isCollpased={isSidebarCollapsed}
+        />
+      </div>
       {/* Footer */}
       <div>
-        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+        <p
+          className={`${
+            isSidebarCollapsed ? "hidden" : "block"
+          } text-center text-xs text-gray-500 py-4`}
+        >
+          &copy; 2024 Edstock
+        </p>
       </div>
     </div>
   );
