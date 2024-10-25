00ccc9727068e0fc2909b69b59a1d4e58e6c116e
Caio <caiomarianni@gmail.com>
2024-10-25 15:15:03 -0300

    CardSalesSummary not working

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -10,15 +10,15 @@ import {
 // import CardExpenseSummary from "./CardExpenseSummary";
 import CardPopularProducts from "./CardPopularProducts";
 // import CardPurchaseSummary from "./CardPurchaseSummary";
-// import CardSalesSummary from "./CardSalesSummary";
+import CardSalesSummary from "./CardSalesSummary";
 // import StatCard from "./StatCard";
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
-      {/* <CardSalesSummary />
-      <CardPurchaseSummary />
+      <CardSalesSummary />
+      {/* <CardPurchaseSummary />
       <CardExpenseSummary />
       <StatCard
         title="Customer & Expenses"
